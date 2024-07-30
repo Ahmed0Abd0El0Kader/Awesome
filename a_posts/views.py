9ab55039bcad643c89182a2d085f07a6dcf5f04d
Abd El Kader <ahmedmohammmed223@gmail.com>
2024-07-30 15:08:42 +0300

    Customize models and edit it

--- a_posts/views.py
+++ a_posts/views.py
@@ -4,10 +4,30 @@ from bs4 import BeautifulSoup
 import requests
 from django.contrib import messages
 from .forms import *
-# Create your views here.
-def home_view(request):
-    posts = Post.objects.all()
-    return render(request, "a_posts/home.html",{'posts': posts})
+
+
+
+
+
+
+
+
+def home_view(request,tag = None):
+    if tag :
+        posts = Post.objects.filter(tags__slug = tag)
+        tag = get_object_or_404(Tag,slug = tag)
+    else:
+        posts = Post.objects.all()
+    categories =  Tag.objects.all()   
+    context = {
+        'posts' : posts,
+        'categories' : categories ,
+        'tag' : tag ,
+    }    
+    return render(request, "a_posts/home.html",context)
+
+
+
 
 
 
@@ -32,6 +52,7 @@ def post_create_view(request):
             post.artist = artist
             
             post.save()
+            form.save_m2m()
             return redirect('home')
     context = {'form': form}
     return render(request, "a_posts/post_create.html",context)
